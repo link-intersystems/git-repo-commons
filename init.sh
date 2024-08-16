4bb6644ad70da45c2ddc8f758516ed08985e107b
René Link <rene.link@link-intersystems.com>
2024-08-16 07:57:04 +0200

    Always execute in the git root dir.

--- init.sh
+++ init.sh
@@ -2,9 +2,14 @@
 
 REPO_TYPE=${1:=gradle}
 
+pushd "$(pwd)"
+cd "$(git rev-parse --show-toplevel)"
+
 git remote add git-repo-commons https://github.com/link-intersystems/git-repo-commons.git
 git remote set-branches git-repo-commons "${REPO_TYPE}"'/*'
 git fetch git-repo-commons
 
 git subtree -P .bin add --squash git-repo-commons/${REPO_TYPE}/bin
 git subtree -P .github add --squash git-repo-commons/${REPO_TYPE}/workflows
+
+popd
