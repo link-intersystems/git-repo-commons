79e38bf4e46f8b858e3eae5695b7eb87601150f2
René Link <rene.link@link-intersystems.com>
2024-08-18 17:09:33 +0200

    Do not forced pushs.

--- updateSubtreeBranches.sh
+++ updateSubtreeBranches.sh
@@ -3,10 +3,11 @@
 git remote set-url origin https://${PUSH_TOKEN}@github.com/link-intersystems/git-repo-commons.git
 git fetch -f --all
 
-git branch gradle/bin git-repo-commons gradle/bin
+if ! git branch --list gradle/bin; then git branch gradle/bin git-repo-commons/gradle/bin; fi
 git subtree split -P gradle/.bin/ -b gradle/bin
 
-git branch gradle/github git-repo-commons gradle/github
+
+if ! git branch --list gradle/github; then git branch gradle/github git-repo-commons/gradle/github; fi
 git subtree split -P gradle/.github/ -b gradle/github
 
 git push origin refs/heads/gradle/*:refs/heads/gradle/*
