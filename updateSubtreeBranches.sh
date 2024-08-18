1620adbe731db50bab6266fd9f3ace51796ce27f
René Link <rene.link@link-intersystems.com>
2024-08-18 12:07:51 +0200

    Removed push action plugin.

--- updateSubtreeBranches.sh
+++ updateSubtreeBranches.sh
@@ -2,3 +2,7 @@
 
 git subtree split -P gradle/.bin/ -b gradle/bin
 git subtree split -P gradle/.github/ -b gradle/github
+
+git remote set-url origin https://${PUSH_TOKEN}@github.com/link-intersystems/git-repo-commons.git
+git fetch --all
+git push -f origin +refs/heads/gradle/*:refs/heads/gradle/*
