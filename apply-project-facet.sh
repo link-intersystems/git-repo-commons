2b3a94cd08c348baf0e2c15e043a9fe74852f395
René Link <rene.link@link-intersystems.com>
2024-08-19 12:41:04 +0200

    Moved almost all code to the bin scripts.

--- apply-project-facet.sh
+++ apply-project-facet.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-
 main() {
   local projectFacet="${1:-gradle}"
 
@@ -10,28 +7,14 @@ main() {
 }
 
 
-
 function installProjectFacet() {
   local projectFacet="$1"
 
-  importGitBinScript "${projectFacet}" git_utils
-  local binHead=$(git fetch --porcelain  https://github.com/link-intersystems/git-repo-commons.git ${projectFacet}/bin | awk '{ print $3 }')
+  local binHead=$(git fetch --porcelain https://github.com/link-intersystems/git-repo-commons.git ${projectFacet}/bin | awk '{ print $3 }')
   local projectFacetScriptId=$(git ls-tree ${binHead} project-facet | awk '{ print $3 }')
 
   cat <(git cat-file -p ${projectFacetScriptId}) | bash -s -- install "${projectFacet}"
 }
-
-function importGitBinScript() {
-  local projectFacet="$1"
-  local scriptpath="$2"
-
-  local binHead=$(git fetch --porcelain  https://github.com/link-intersystems/git-repo-commons.git ${projectFacet}/bin | awk '{ print $3 }')
-  local gitUtilsObjectId=$(git ls-tree ${binHead} ${scriptpath} | awk '{ print $3 }')
-
-  source <(git cat-file -p ${gitUtilsObjectId})
-}
-
-
 
 
 main "$@"; exit
