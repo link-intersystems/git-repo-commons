5c253aaae52110cebf456b30419534459629309a
René Link <rene.link@link-intersystems.com>
2024-08-19 12:18:43 +0200

    Moved almost all code to the bin scripts.

--- apply-project-facet.sh
+++ apply-project-facet.sh
@@ -2,52 +2,35 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+
 main() {
   local projectFacet="${1:-gradle}"
-  onGitRootDir applyProjectFacet "git-repo-commons" "${projectFacet}"
+
+  installProjectFacet "${projectFacet}"
 }
 
-function onGitRootDir() {
-  pushd "$(pwd)" > /dev/null
-  cd "$(git rev-parse --show-toplevel)"
-  local command=$1
-  shift
-  $command $*
-  popd > /dev/null
+
+
+function installProjectFacet() {
+  local projectFacet="$1"
+
+  importGitBinScript "${projectFacet}" git_utils
+  local projectFacetScriptId=$(git ls-tree $https://github.com/link-intersystems/${repo}.git ${projectFacet}/bin project-facet | awk '{ print $3 }')
+
+  cat <(git cat-file -p ${projectFacetScriptId}) | bash -s -- install "${projectFacet}"
 }
 
-function updateSubtree(){
-  local repo=$1
-  local projectFacet=$2
-  local subtree=$3
-  local existingSubtrees=$(git log | grep git-subtree-dir | tr -d ' ' | cut -d ":" -f2 | sort | uniq | xargs -I {} bash -c 'if [ -d $(git rev-parse --show-toplevel)/{} ] ; then echo {}; fi')
-  local subtreeBranchQualifier="${subtree#\.}"
+function importGitBinScript() {
+  local projectFacet="$1"
+  local scriptpath="$2"
 
-  if [[ ${existingSubtrees} =~ ${subtree} ]]; then
-    git subtree -P "${subtree}" pull --squash "${repo}" "${projectFacet}/${subtreeBranchQualifier}"
-  else
-    local subtreeBranchQualifier="${subtree#\.}"
-    git subtree -P "${subtree}" add --squash "${repo}/${projectFacet}/${subtreeBranchQualifier}"
-  fi
+  local binHead=$(git fetch --porcelain  https://github.com/link-intersystems/git-repo-commons.git ${projectFacet}/bin | awk '{ print $3 }')
+  local gitUtilsObjectId=$(git ls-tree ${binHead} ${scriptpath} | awk '{ print $3 }')
+
+  source <(git cat-file -p ${gitUtilsObjectId})
 }
 
-function applyProjectFacet() {
-  local repo="$1"
-  local projectFacet="$2"
 
-  echo "Applying project facet ${projectFacet}"
-
-  if [ -z "$(git remote | grep "${repo}")" ]; then
-    echo "Adding Repository ${repo}"
-    git remote add ${repo} https://github.com/link-intersystems/${repo}.git
-  fi
-
-  echo "Fetching refs/remotes/${repo}/${projectFacet}/*"
-  git fetch ${repo} +refs/heads/${projectFacet}/*:refs/remotes/${repo}/${projectFacet}/*
-
-  updateSubtree "${repo}" "${projectFacet}" ".bin"
-  updateSubtree "${repo}" "${projectFacet}" ".github"
-}
 
 
 main "$@"; exit
